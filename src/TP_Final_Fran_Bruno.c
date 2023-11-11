df78efa4f78b4baa106d6ee038485dfd4d51c8b9
BrunoGugli <brunoguglielmotti@gmail.com>
2023-11-11 05:20:59 -0300

    PWMs - ADC - DAC
    PWMs funcionan. Probar ADC y DAC

--- src/TP_Final_Fran_Bruno.c
+++ src/TP_Final_Fran_Bruno.c
@@ -7,19 +7,23 @@
 #include "lpc17xx_timer.h"
 #include "lpc17xx_uart.h"
 #include "lpc17xx_pinsel.h"
+#include "lpc17xx_systick.h"
 
 void conf_GPIO(void);
 void conf_EXTI0(void);
 void conf_EXTI1(void);
 void conf_ADC(void);
 void conf_DAC(void);
-void conf_DMA(uint8_t P2M);
+void conf_DMA(void);
 void configUART(void);
-void initPWMs(void);
+void conf_Timer0(void);
+void conf_Timer1(void);
+void conf_Timer2(void);
+void conf_SysTick(void);
 
-void conf_PWM_Red();
-void conf_PWM_Green();
-void conf_PWM_Blue();
+void conf_PWM_Red(uint8_t redVal);
+void conf_PWM_Green(uint8_t greenVal);
+void conf_PWM_Blue(uint8_t blueVal);
 
 void turn_on_PWMs(void);
 void turn_off_PWMs(void);
@@ -32,8 +36,10 @@ void delay(uint32_t times);
  * ADC_Freq = 1502 [Hz] -> T between samples = 666[us].
  * Then that must be the DAC timeout -> T_ticks = PCLK * T_out -> T_ticks = 25[MHz] * 666[us] = 16644.47;
  */
-#define ADC_FREQ 1502
-#define DAC_TOUT ((1/ADC_FREQ)*25000000)
+#define ADC_FREQ 200000
+#define SAMPLES_FREQ 1000
+#define CANT_SAMPLES (5*SAMPLES_FREQ)
+#define DAC_TOUT ((1/SAMPLES_FREQ)*25000000)
 
 #define REDLED_LPC		(1<<22)
 #define GREENLED_LPC 	(1<<25)
@@ -41,33 +47,46 @@ void delay(uint32_t times);
 
 #define UART_RX_BUFFER_SIZE 4
 
-uint32_t led_signal[4096]; //In a range of 2.7 seconds, 4096 will be loaded with a rate of 1502[Hz] (1 data in 666[us])
-uint8_t adc_converting = 0;
-uint8_t mode = 0; //0: RGB lights controlled by UART from PC. 1: ADC signal recreation
+uint32_t led_signal[CANT_SAMPLES]; //In a range of 5 seconds, 5000 data will be loaded with a rate of 1000[Hz] (1 data in 1[ms])
 uint8_t uartRxBuffer[4] = "";
 
+uint8_t adc_converting = 0;
+uint8_t mode = 0; //0: RGB lights controlled by UART from PC. 1: ADC signal recreation
+uint16_t data_counter = 0;
+uint16_t one_sec_check = 0;
+uint8_t adc_preparing = 0;
+
+//Match values for PWMs duty cycles
+uint8_t current_red_pwm_duty = 100 - 1;
+uint8_t current_green_pwm_duty = 50 - 1;
+uint8_t current_blue_pwm_duty = 50 - 1;
+
 GPDMA_LLI_Type DMA_list;
+
 int main(void) {
-
-
 	conf_GPIO();
+
+	conf_SysTick();
 	conf_EXTI0();
 	conf_EXTI1();
 	conf_ADC();
 	conf_DAC();
 	configUART();
-	//init_PWMs();//implement
+	conf_DMA();
 
-	//Set P0.3 for adc signal
+	conf_Timer0();//Timer for red led PWM
+	conf_Timer1();//Timer for green led PWM
+	conf_Timer2();//Timer for blue led PWM
+
+	//Set P0.3 for ADC signal
 	GPIO_SetValue(0, 0x8);
 
-	//UART_TxCmd(LPC_UART2, ENABLE);
-	//UART_IntConfig(LPC_UART2, UART_INTCFG_RBR, ENABLE);
 	GPIO_SetValue(3, GREENLED_LPC);
-	NVIC_EnableIRQ(UART2_IRQn);
 
+	NVIC_EnableIRQ(UART2_IRQn);
 	NVIC_EnableIRQ(EINT0_IRQn);
 	NVIC_EnableIRQ(EINT1_IRQn);
+	turn_on_PWMs();
 
     while(1) {
 
@@ -101,7 +120,6 @@ void conf_GPIO(void){
 	//Set P0.0 P0.1 P0.2 and P0.3 as output
 	GPIO_SetDir(0,0b1111,1);
 
-
 	//Also we configure the integrated led's for user alerts
 	//P0.22 as GPIO for red led
 	pinc.Pinnum = PINSEL_PIN_22;
@@ -123,6 +141,63 @@ void conf_GPIO(void){
 	//Shut down led's
 	LPC_GPIO0->FIOSET |= REDLED_LPC;
 	LPC_GPIO3->FIOSET |= (GREENLED_LPC | BLUELED_LPC);
+}
+
+void conf_Timer0(void){
+	TIM_TIMERCFG_Type timc;
+	timc.PrescaleOption=TIM_PRESCALE_TICKVAL;
+	timc.PrescaleValue = 2500; //Tr = 100[us]
+
+	TIM_MATCHCFG_Type matc;
+	matc.MatchChannel = 0;
+	matc.IntOnMatch = ENABLE;
+	matc.ResetOnMatch = ENABLE;
+	matc.StopOnMatch = DISABLE;
+	matc.ExtMatchOutputType =TIM_EXTMATCH_NOTHING;
+	matc.MatchValue = (uint32_t)(current_red_pwm_duty);
+
+	TIM_Init(LPC_TIM0,TIM_TIMER_MODE,&timc);
+	TIM_ConfigMatch(LPC_TIM0, &matc);
+}
+
+void conf_Timer1(void){
+	TIM_TIMERCFG_Type timc;
+	timc.PrescaleOption=TIM_PRESCALE_TICKVAL;
+	timc.PrescaleValue = 2500; //Tr = 100[us]
+
+	TIM_MATCHCFG_Type matc;
+	matc.MatchChannel = 0;
+	matc.IntOnMatch = ENABLE;
+	matc.ResetOnMatch = ENABLE;
+	matc.StopOnMatch = DISABLE;
+	matc.ExtMatchOutputType =TIM_EXTMATCH_NOTHING;
+	matc.MatchValue = (uint32_t)(current_green_pwm_duty);
+
+	TIM_Init(LPC_TIM1,TIM_TIMER_MODE,&timc);
+	TIM_ConfigMatch(LPC_TIM1, &matc);
+}
+
+void conf_Timer2(void){
+	TIM_TIMERCFG_Type timc;
+	timc.PrescaleOption=TIM_PRESCALE_TICKVAL;
+	timc.PrescaleValue = 2500; //Tr = 100[us]
+
+	TIM_MATCHCFG_Type matc;
+	matc.MatchChannel = 0;
+	matc.IntOnMatch = ENABLE;
+	matc.ResetOnMatch = ENABLE;
+	matc.StopOnMatch = DISABLE;
+	matc.ExtMatchOutputType =TIM_EXTMATCH_NOTHING;
+	matc.MatchValue = (uint32_t)(current_blue_pwm_duty);
+
+	TIM_Init(LPC_TIM2,TIM_TIMER_MODE,&timc);
+	TIM_ConfigMatch(LPC_TIM2, &matc);
+}
+
+void conf_SysTick(void){
+	SysTick->CTRL |= (1<<2);
+	SysTick->LOAD = ((1/SAMPLES_FREQ)*SystemCoreClock) - 1;
+	SysTick->VAL = 0;
 }
 
 //External interrupt configuration for general modes changing
@@ -170,10 +245,8 @@ void conf_ADC(void){
 	PINSEL_ConfigPin(&pinc);
 
 	ADC_Init(LPC_ADC,ADC_FREQ);
-	ADC_IntConfig(LPC_ADC,ADC_ADGINTEN,ENABLE);//Enable interrupt for DMA request
+	ADC_IntConfig(LPC_ADC,ADC_ADINTEN0,ENABLE);//Enable interrupt for DMA request
 	//Since this is a previous configuration, the channel will be disabled until we need the ADC (exti0 handler).
-	ADC_BurstCmd(LPC_ADC,1);//Burst mode for DMA
-	ADC_StartCmd(LPC_ADC,ADC_START_CONTINUOUS);
 }
 
 void conf_DAC(void){
@@ -195,33 +268,29 @@ void conf_DAC(void){
 	DAC_UpdateValue(LPC_DAC, 0);
 }
 
-//DMA configuration function. 'P2M' is a flag to know if the transfer is ADC to Mem or Mem to DAC.
-void conf_DMA(uint8_t P2M){
+void conf_DMA(void){
 	NVIC_DisableIRQ(DMA_IRQn);
-	if(!P2M){//If transfer type is M2P (DAC to memory), then configure LLI
-		DMA_list.SrcAddr = (uint32_t)led_signal;
-		DMA_list.DstAddr = (uint32_t)&(LPC_DAC->DACR);
-		DMA_list.NextLLI = (uint32_t)&DMA_list;
-		DMA_list.Control = (sizeof(led_signal) - 1)	//Transfer size
-					  |(2<<18)	//Source width = 32 bits
-					  |(2<<21);	//Destination width = 32 bits
-		//Set SI and clear DI
-		DMA_list.Control &= ~(1<<27);
-		DMA_list.Control |= (1<<26);
-	}
+
+	DMA_list.SrcAddr = (uint32_t)led_signal;
+	DMA_list.DstAddr = (uint32_t)&(LPC_DAC->DACR);
+	DMA_list.NextLLI = (uint32_t)&DMA_list;
+	DMA_list.Control = (sizeof(led_signal) - 1)	//Transfer size
+				  |(2<<18)	//Source width = 32 bits
+				  |(2<<21)	//Destination width = 32 bits
+				  |(1<<26);	//Set SI
 
 	GPDMA_Init();
 
 	GPDMA_Channel_CFG_Type dmac;
 	dmac.ChannelNum = 0;
-	dmac.SrcMemAddr = (uint32_t)((P2M) ? 0 : led_signal);
-	dmac.DstMemAddr = (uint32_t)((P2M) ? led_signal : 0);
+	dmac.SrcMemAddr = (uint32_t)(led_signal);
+	dmac.DstMemAddr = 0;
 	dmac.TransferSize = (sizeof(led_signal) - 1);
 	dmac.TransferWidth = 0;
-	dmac.TransferType = (P2M) ? GPDMA_TRANSFERTYPE_P2M : GPDMA_TRANSFERTYPE_M2P;
-	dmac.SrcConn = (P2M) ? GPDMA_CONN_ADC : 0;
-	dmac.DstConn = (P2M) ? 0 : GPDMA_CONN_DAC;
-	dmac.DMALLI = (uint32_t)((P2M) ? 0 : &DMA_list);
+	dmac.TransferType = GPDMA_TRANSFERTYPE_M2P;
+	dmac.SrcConn = 0;
+	dmac.DstConn = GPDMA_CONN_DAC;
+	dmac.DMALLI = (uint32_t)&(DMA_list);
 	GPDMA_Setup(&dmac);
 	LPC_GPDMACH0->DMACCConfig &= ~(1<<18); //Enable requests for channel 0
 }
@@ -256,31 +325,26 @@ void configUART(void) {
 }
 
 void UART2_IRQHandler(void) {
-	GPIO_ClearValue(3, GREENLED_LPC);
 	uint32_t intsrc, tmp, tmp1;
-		//Determina la fuente de interrupci�n
-		intsrc = UART_GetIntId(LPC_UART2);
-		tmp = intsrc & UART_IIR_INTID_MASK;
-		// Eval�a Line Status
-		if (tmp == UART_IIR_INTID_RLS){
-			tmp1 = UART_GetLineStatus(LPC_UART2);
-			tmp1 &= (UART_LSR_OE | UART_LSR_PE | UART_LSR_FE \
-					| UART_LSR_BI | UART_LSR_RXFE);
-			// ingresa a un Loop infinito si hay error
-			if (tmp1) {
-				while(1){};
-			}
+	//Determina la fuente de interrupci�n
+	intsrc = UART_GetIntId(LPC_UART2);
+	tmp = intsrc & UART_IIR_INTID_MASK;
+	// Eval�a Line Status
+	if (tmp == UART_IIR_INTID_RLS){
+		tmp1 = UART_GetLineStatus(LPC_UART2);
+		tmp1 &= (UART_LSR_OE | UART_LSR_PE | UART_LSR_FE \
+				| UART_LSR_BI | UART_LSR_RXFE);
+		// ingresa a un Loop infinito si hay error
+		if (tmp1) {
+			while(1){};
 		}
-		// Receive Data Available or Character time-out
-		if ((tmp == UART_IIR_INTID_RDA) || (tmp == UART_IIR_INTID_CTI)){
-			UART_Receive(LPC_UART2, uartRxBuffer, sizeof(uartRxBuffer), BLOCKING);
-		}
-		GPIO_SetValue(3, GREENLED_LPC);
-		return;
-}
-
-void processReceivedData(void){
-
+	}
+	// Receive Data Available or Character time-out
+	if ((tmp == UART_IIR_INTID_RDA) || (tmp == UART_IIR_INTID_CTI)){
+		UART_Receive(LPC_UART2, uartRxBuffer, sizeof(uartRxBuffer), BLOCKING);
+	}
+	processReceivedData();
+	return;
 }
 
 void EINT0_IRQHandler(void){
@@ -294,17 +358,15 @@ void EINT0_IRQHandler(void){
 		//Shutdown PWM signals on GPIO pins
 		turn_off_PWMs();
 
+		//Enable SysTick
+		//GPIO_SetValue(3, GREENLED_LPC);
+		SysTick->CTRL |= (0x3);
 
-		//Configure DMA and enable its channel and ADC channel
-		conf_DMA(1);
-		NVIC_EnableIRQ(DMA_IRQn);
-		GPDMA_ChannelCmd(0, ENABLE);
-		ADC_ChannelCmd(LPC_ADC, ADC_CHANNEL_0, ENABLE);
-		adc_converting = 1;
+		adc_preparing = 1;
 		mode = 1;
 	}
 	else{
-		if(adc_converting){
+		if(adc_converting || adc_preparing){
 			//Alert to user and do nothing
 			LPC_GPIO0->FIOCLR |= REDLED_LPC;
 			delay(2000);
@@ -334,9 +396,9 @@ void EINT0_IRQHandler(void){
 	return;
 }
 
-void EINT1_IRQHandler(){
+void EINT1_IRQHandler(void){
 	if(mode == 1){
-		if(adc_converting){
+		if(adc_converting || adc_preparing){
 			//Alert to user and do nothing
 			LPC_GPIO0->FIOCLR |= REDLED_LPC;
 			delay(2000);
@@ -346,12 +408,10 @@ void EINT1_IRQHandler(){
 			LPC_GPDMACH0->DMACCConfig |= (1<<18); //Disable requests for channel 0
 			while(LPC_GPDMACH0->DMACCConfig & (1<<17)); //Wait for possible data in channels FIFO
 			GPDMA_ChannelCmd(0,DISABLE);
-			conf_DMA(1);
 
-			GPDMA_ChannelCmd(0, ENABLE);
-			NVIC_EnableIRQ(DMA_IRQn);
-			ADC_ChannelCmd(LPC_ADC, ADC_CHANNEL_0, ENABLE);
-			adc_converting = 1;
+			SysTick->CTRL |= (0x3);
+
+			adc_preparing = 1;
 		}
 	}
 	else{
@@ -365,32 +425,145 @@ void EINT1_IRQHandler(){
 	EXTI_ClearEXTIFlag(EXTI_EINT1);
 }
 
-void DMA_IRQHandler(){
-	if(GPDMA_IntGetStatus(GPDMA_STAT_INTTC, 0)){
-		if(adc_converting){
-			while(!ADC_GlobalGetStatus(LPC_ADC,ADC_DATA_DONE));//Wait for ADC to finish
-			ADC_ChannelCmd(LPC_ADC, ADC_CHANNEL_0, DISABLE);
+void ADC_IRQHandler(void){
+	ADC_ChannelCmd(LPC_ADC, ADC_CHANNEL_0, DISABLE);
+	if(data_counter < CANT_SAMPLES){
+		led_signal[data_counter] = LPC_ADC->ADDR0;
+		data_counter++;
+	}
+	else{
+		data_counter = 0;
+		NVIC_DisableIRQ(ADC_IRQn);
+		adapt_signal_data_for_dac();
+		adc_converting = 0;
+		GPIO_SetValue(3, GREENLED_LPC);
+		SysTick->CTRL &= ~(0x3); //Disable SysTick
 
-			LPC_GPDMACH0->DMACCConfig &= ~(1<<18); //Disable requests for channel 0
-			while(LPC_GPDMACH0->DMACCConfig & (1<<17)); //Wait for possible data in channels FIFO
-			GPDMA_ChannelCmd(0,DISABLE);
-
-			adapt_signal_data_for_dac();
-			conf_DMA(0);//Configure DMA for DAC to memory transfer
-			GPDMA_ChannelCmd(0, ENABLE);
-			adc_converting = 0;
-			//NVIC_DisableIRQ(DMA_IRQn);//Disable DMA interrupts
-		}
+		GPDMA_ChannelCmd(0, ENABLE);
 	}
 
-	GPDMA_ClearIntPending(GPDMA_STATCLR_INTTC, 0);
-	GPDMA_ClearIntPending(GPDMA_STATCLR_INTERR, 0);
+	LPC_ADC->ADGDR &= LPC_ADC->ADGDR;
+	LPC_ADC->ADDR0;
+}
+
+void SysTick_Handler(void){
+	if(adc_preparing){
+		if(one_sec_check<1000){
+			one_sec_check++;
+		}
+		else{
+			adc_preparing = 0;
+			one_sec_check = 0;
+			adc_converting = 1;
+			NVIC_EnableIRQ(ADC_IRQn);
+			ADC_ChannelCmd(LPC_ADC, ADC_CHANNEL_0, ENABLE);
+			ADC_StartCmd(LPC_ADC, ADC_START_NOW);
+			GPIO_ClearValue(3, GREENLED_LPC);
+		}
+	}
+	else{
+		ADC_ChannelCmd(LPC_ADC, ADC_CHANNEL_0, ENABLE);
+		ADC_StartCmd(LPC_ADC, ADC_START_NOW);
+	}
+
+	SysTick->CTRL &= SysTick->CTRL;//Clear flag
+}
+
+void TIMER0_IRQHandler(void){
+	//TIM_Cmd(LPC_TIM0,DISABLE);
+	LPC_TIM0->TCR |= 2;
+	if(LPC_GPIO0->FIOPIN & (0x1)){
+		if(current_red_pwm_duty != 99){
+			LPC_GPIO0->FIOCLR |= 1;
+			LPC_TIM0->MR0 = 99 - current_red_pwm_duty - 1;
+		}
+	}
+	else{
+		LPC_GPIO0->FIOSET |= 1;
+		LPC_TIM0->MR0 = (uint32_t)(current_red_pwm_duty);
+	}
+	//TIM_Cmd(LPC_TIM0,ENABLE);
+    LPC_TIM0->TCR &= ~(2);
+
+	TIM_ClearIntPending(LPC_TIM0,TIM_MR0_INT);
+}
+
+void TIMER1_IRQHandler(void){
+	//TIM_Cmd(LPC_TIM0,DISABLE);
+	 LPC_TIM1->TCR |= 2;
+	if(LPC_GPIO0->FIOPIN & (0x2)){
+		if(current_green_pwm_duty != 99){
+			LPC_GPIO0->FIOCLR |= 2;
+			LPC_TIM1->MR0 = 99 - current_green_pwm_duty - 1;
+		}
+	}
+	else{
+		LPC_GPIO0->FIOSET |= 2;
+		LPC_TIM1->MR0 = (uint32_t)(current_green_pwm_duty);
+	}
+	//TIM_Cmd(LPC_TIM0,ENABLE);
+    LPC_TIM1->TCR &= ~(2);
+
+	TIM_ClearIntPending(LPC_TIM1,TIM_MR0_INT);
+}
+
+void TIMER2_IRQHandler(void){
+	//TIM_Cmd(LPC_TIM0,DISABLE);
+	 LPC_TIM2->TCR |= 2;
+	if(LPC_GPIO0->FIOPIN & (0x4)){
+		if(current_blue_pwm_duty != 99){
+			LPC_GPIO0->FIOCLR |= 4;
+			LPC_TIM2->MR0 = 99 - current_blue_pwm_duty - 1;
+		}
+	}
+	else{
+		LPC_GPIO0->FIOSET |= 4;
+		LPC_TIM2->MR0 = (uint32_t)(current_blue_pwm_duty);
+	}
+	//TIM_Cmd(LPC_TIM0,ENABLE);
+    LPC_TIM2->TCR &= ~(2);
+
+	TIM_ClearIntPending(LPC_TIM2,TIM_MR0_INT);
 }
 
 void adapt_signal_data_for_dac(void){
 	for(int i =0 ; i<sizeof(led_signal);i++){
-		led_signal[i] &= (0x3FF << 6);
+		led_signal[i] &= (0xFF << 8);
 	}
+}
+
+void processReceivedData(void){
+	uint8_t info, blueVal, greenVal, redVal;
+	blueVal = uartRxBuffer[0];
+	greenVal = uartRxBuffer[1];
+	redVal = uartRxBuffer[2];
+	info = uartRxBuffer[3];
+
+	conf_PWM_Red(redVal);
+	conf_PWM_Green(greenVal);
+	conf_PWM_Blue(blueVal);
+}
+
+void conf_PWM_Red(uint8_t redVal){
+	current_red_pwm_duty = ((redVal*100/255) - 1);
+}
+
+void conf_PWM_Green(uint8_t greenVal){
+	current_green_pwm_duty = ((greenVal*100/255) - 1);
+}
+
+void conf_PWM_Blue(uint8_t blueVal){
+	current_blue_pwm_duty = ((blueVal*100/255) - 1);
+}
+
+void turn_on_PWMs(void){
+	NVIC_EnableIRQ(TIMER0_IRQn);
+	NVIC_EnableIRQ(TIMER1_IRQn);
+	NVIC_EnableIRQ(TIMER2_IRQn);
+
+	TIM_Cmd(LPC_TIM0,ENABLE);
+	TIM_Cmd(LPC_TIM1,ENABLE);
+	TIM_Cmd(LPC_TIM2,ENABLE);
 }
 
 void turn_off_PWMs(void){
@@ -398,21 +571,10 @@ void turn_off_PWMs(void){
 	NVIC_DisableIRQ(TIMER1_IRQn);
 	NVIC_DisableIRQ(TIMER2_IRQn);
 
-	//TIM_Cmd(LPC_TIM0,DISABLE);
-	//TIM_Cmd(LPC_TIM1,DISABLE);
-	//TIM_Cmd(LPC_TIM2,DISABLE);
+	TIM_Cmd(LPC_TIM0,DISABLE);
+	TIM_Cmd(LPC_TIM1,DISABLE);
+	TIM_Cmd(LPC_TIM2,DISABLE);
 	GPIO_ClearValue(0, 0x7);
-}
-
-
-void turn_on_PWMs(void){
-	NVIC_EnableIRQ(TIMER0_IRQn);
-	NVIC_EnableIRQ(TIMER1_IRQn);
-	NVIC_EnableIRQ(TIMER2_IRQn);
-
-	//TIM_Cmd(LPC_TIM0,ENABLE);
-	//TIM_Cmd(LPC_TIM1,ENABLE);
-	//TIM_Cmd(LPC_TIM2,ENABLE);
 }
 
 void delay(uint32_t times) {
